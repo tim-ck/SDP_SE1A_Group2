a3588a356fb2d66f30426de751cc1e65af346110
PisGood <50736546+PisGood@users.noreply.github.com>
2020-06-09 15:14:38 +0800

    +pong forom @vs

--- Tenant/TenantRentShowcase.cs
+++ Tenant/TenantRentShowcase.cs
@@ -8,12 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-<<<<<<< Updated upstream
-namespace Form2
-=======
-namespace SDP_SE1A_Group2
->>>>>>> Stashed changes
-{
+namespace SDP_SE1A_Group2 { 
+
     public partial class TenantRentShowcase : Form
     {
         public TenantRentShowcase()
