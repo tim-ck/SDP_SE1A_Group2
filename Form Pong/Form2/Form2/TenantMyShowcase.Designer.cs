949c6fc8bbf2e1b5c06b760ba2d48645f0697bd5
PisGood <50736546+PisGood@users.noreply.github.com>
2020-06-09 14:23:24 +0800

    Add Pong Form

---
+++ Form Pong/Form2/Form2/TenantMyShowcase.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Form2
+{
+    partial class TenantMyShowcase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TenantMyShowcase));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.location = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.showcaseId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dueDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.extendLease = new System.Windows.Forms.DataGridViewImageColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.location,
+            this.showcaseId,
+            this.dueDate,
+            this.extendLease});
+            this.dataGridView1.Location = new System.Drawing.Point(54, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(593, 325);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // location
+            // 
+            this.location.HeaderText = "Location";
+            this.location.MinimumWidth = 6;
+            this.location.Name = "location";
+            this.location.Width = 125;
+            // 
+            // showcaseId
+            // 
+            this.showcaseId.HeaderText = "Showcase ID";
+            this.showcaseId.MinimumWidth = 6;
+            this.showcaseId.Name = "showcaseId";
+            this.showcaseId.Width = 80;
+            // 
+            // dueDate
+            // 
+            this.dueDate.HeaderText = "Due Date";
+            this.dueDate.MinimumWidth = 6;
+            this.dueDate.Name = "dueDate";
+            this.dueDate.Width = 125;
+            // 
+            // extendLease
+            // 
+            this.extendLease.HeaderText = "Extend Lease";
+            this.extendLease.Image = global::Form2.Properties.Resources.add_time;
+            this.extendLease.MinimumWidth = 6;
+            this.extendLease.Name = "extendLease";
+            this.extendLease.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "picBack";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(78, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Back";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridViewImageColumn1
+            // 
+            this.dataGridViewImageColumn1.HeaderText = "Extend Lease";
+            this.dataGridViewImageColumn1.Image = global::Form2.Properties.Resources.add_time;
+            this.dataGridViewImageColumn1.MinimumWidth = 6;
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            this.dataGridViewImageColumn1.Width = 125;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleName = "picBack";
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.picBack_Click);
+            // 
+            // Form2MyShowcase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 648);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2MyShowcase";
+            this.Text = "My Showcase";
+            this.Load += new System.EventHandler(this.Form2MyShowcase_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn location;
+        private System.Windows.Forms.DataGridViewTextBoxColumn showcaseId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dueDate;
+        private System.Windows.Forms.DataGridViewImageColumn extendLease;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn1;
+    }
+}
---
+++ TenantMyShowcase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form2
+{
+    public partial class TenantMyShowcase : Form
+    {
+        public TenantMyShowcase()
+        {
+            InitializeComponent();
+        }
+
+        private void picBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+          
+        }
+
+        private void Form2MyShowcase_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
